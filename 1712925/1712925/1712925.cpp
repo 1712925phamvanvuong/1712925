d55f8c76f93566cfa8620736caa44d72098bd1e6
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2018-04-01 10:51:09 +0700

    1712925

---
+++ 1712925/1712925/1712925.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<wchar.h>
+#include<stdlib.h>
+struct sinhvien
+{
+	wchar_t mssv[10];
+	wchar_t hovaten[30];
+	wchar_t khoc[30];
+	int khoa;
+	wchar_t ngaysinh[12];
+	wchar_t hinhcanhan[16];
+	wchar_t* motabanthan;
+	wchar_t* sothichx;
+};
+
