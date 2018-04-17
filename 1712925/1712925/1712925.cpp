be29b001bd3dddd064d51266d6b6db7f797a020a
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2018-04-18 01:16:26 +0700

    chinhsua

--- 1712925/1712925/1712925.cpp
+++ 1712925/1712925/1712925.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<string.h>
 #include <conio.h>
 #include<wchar.h>
@@ -6,18 +6,27 @@
 #include <fcntl.h> 
 #include <io.h> 
 
-
 struct sinhvien
 {
-	wchar_t mssv[30];
-	wchar_t hovaten[30];
-	wchar_t khoc[30];
-	wchar_t khoa[30];
-	wchar_t ngaysinh[30];
-	wchar_t hinhcanhan[30];
-	wchar_t motabanthan[30];
-	wchar_t sothichx[30];
+	wchar_t* mssv;
+	wchar_t* hovaten;
+	wchar_t* khoc;
+	wchar_t* khoa;
+	wchar_t* ngaysinh;
+	wchar_t* hinhcanhan;
+	wchar_t* mail;
+	wchar_t* motabanthan;
+	wchar_t* sothichx;
 };
+
+int len(wchar_t s[])
+{
+	int i = 0;
+	while (s[i] != '\0'){
+		i++;
+	}
+	return i;
+}
 
 int token(FILE* f, wchar_t h)
 {
@@ -34,21 +43,46 @@ int token(FILE* f, wchar_t h)
 	}
 	return i;
 }
-
-void docfile(FILE* fIN, sinhvien &x);
-void docfile(FILE* fIn, sinhvien &x)
+int finding(FILE* f, wchar_t s[])
 {
+	int j, pos = 0;
+	while (!feof(f)){
+		j = 0;
+		fseek(f, pos, SEEK_SET);
+		for (int i = 0; i < len(s); i++){
+			if (fgetwc(f) == s[i]){
+				j++;
+			}
+			else{
+				break;
+			}
+		}
+		if (j == len(s) - 1){
+			return ftell(f);
+		}
+		else{
+			fseek(f, pos + 1, SEEK_SET);
+		}
+		pos++;
+	}
+	return -1;
+}
 
+
+
+void doc(FILE* fIn, sinhvien &x, int &begin)
+{
 	int a = token(fIn, L',');
-
-	fseek(fIn, 3, SEEK_SET);
-	fgetws(x.mssv, a + 1, fIn);
+	x.mssv = (wchar_t*)malloc(sizeof(wchar_t)*(a - 5));
+	fseek(fIn, begin, SEEK_SET);
+	fgetws(x.mssv, a + 2, fIn);
 	int b = ftell(fIn) + 1;
 	wprintf(L"%ls\n", x.mssv);
 
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.hovaten = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.hovaten, a + 2, fIn);
 	b = ftell(fIn) + 1;
@@ -57,6 +91,7 @@ void docfile(FILE* fIn, sinhvien &x)
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.khoa = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.khoa, a + 2, fIn);
 	b = ftell(fIn) + 1;
@@ -64,6 +99,7 @@ void docfile(FILE* fIn, sinhvien &x)
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.khoc = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.khoc, a + 2, fIn);
 	b = ftell(fIn) + 1;
@@ -71,6 +107,7 @@ void docfile(FILE* fIn, sinhvien &x)
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.ngaysinh = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.ngaysinh, a + 2, fIn);
 	b = ftell(fIn) + 1;
@@ -78,6 +115,7 @@ void docfile(FILE* fIn, sinhvien &x)
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.hinhcanhan = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.hinhcanhan, a + 2, fIn);
 	b = ftell(fIn) + 1;
@@ -85,41 +123,63 @@ void docfile(FILE* fIn, sinhvien &x)
 
 	fseek(fIn, b + 1, SEEK_SET);
 	a = token(fIn, L',');
+	x.motabanthan = (wchar_t*)malloc(sizeof(wchar_t)*a);
 	fseek(fIn, b, SEEK_SET);
 	fgetws(x.motabanthan, a + 2, fIn);
-	b = ftell(fIn) + 1;
+	b = ftell(fIn);
 	wprintf(L"%ls\n", x.motabanthan);
 
-	if (fgetwc(fIn) == L',')
-	{
-		fseek(fIn, b + 1, SEEK_SET);
+	if (fgetwc(fIn) == L','){
+		fseek(fIn, b + 2, SEEK_SET);
 		a = token(fIn, NULL);
-		fseek(fIn, b, SEEK_SET);
+		x.sothichx = (wchar_t*)malloc(sizeof(wchar_t)*a);
+		fseek(fIn, b + 1, SEEK_SET);
 		fgetws(x.sothichx, 100, fIn);
 		wprintf(L"%ls\n", x.sothichx);
 	}
+	else{
+		fseek(fIn, b, SEEK_SET);
+	}
 
+
+	begin = ftell(fIn);
 }
-void wmain()
+
+void main()
 {
-	sinhvien x[5];
+
 	_setmode(_fileno(stdout), _O_U16TEXT); //needed for output
 	_setmode(_fileno(stdin), _O_U16TEXT); //needed for input
-
 	FILE* fIn;
-	_wfopen_s(&fIn, L"test.txt", L"r, ccs=UTF-8");
+	_wfopen_s(&fIn, L"Book123.csv", L"r, ccs=UTF-8");
 	if (fIn == NULL)
 	{
-		wprintf(L"khong mo duoc FILE!!!\n");
+		wprintf(L"Không mở được FILE!!!\n");
 	}
 	else
 	{
-		for (int i = 0; i < 5; i++)
-		{
-			docfile(fIn, x[i]);
+		int i = 0, begin = 3;
+		sinhvien* x;
+		fgetwc(fIn);
+		while (!feof(fIn)){
+			i++;
+			x = (sinhvien*)malloc(sizeof(sinhvien)*i);
+			doc(fIn, x[i - 1], begin);
+			fgetwc(fIn);
 		}
+		for (int j = 0; j < i; j++){
+			free(x[j].mssv);
+			free(x[j].hovaten);
+			free(x[j].khoa);
+			free(x[j].khoc);
+			free(x[j].ngaysinh);
+			free(x[j].motabanthan);
+			free(x[j].sothichx);
+		}
+		free(x);
+		fclose(fIn);
 
 	}
-	fclose(fIn);
+
 	_getch();
 }
